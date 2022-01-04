ce502e3192814cbc776dac6170ce0048139b4baa
Christopher Speltz <chrisspeltz@msn.com>
2022-01-03 22:56:15 -0600

    created newRole and newDepartment functions

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const chalk = require("chalk");
 var figlet = require("figlet");
 const db = require("./db/connection");
 
-
 console.log(
   chalk.green(figlet.textSync("Employee CMS", { horizontalLayout: "full" }))
 );
-
 
 // menu options
 const mainMenu = () => {
@@ -39,14 +37,14 @@ const mainMenu = () => {
         case "View All Employees":
           queryEmployees();
           break;
-        // case "Add A Department":
-        //   addDepartment();
-        //   break;
-        // case "Add A Role":
-        //   addRole();
-        //   break;
+        case "Add A Department":
+          newDepartment();
+          break;
+        case "Add A Role":
+          newRole();
+          break;
         // case "Add An Employee":
-        //   addEmployee();
+        //   newEmployee();
         //   break;
         // case "Update An Employee Role":
         //   updateEmployeeRole();
@@ -60,8 +58,8 @@ const mainMenu = () => {
     });
 };
 
-// Query all departments - formatted table showing department names and department ids
-// const allDepartments ?
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
 const queryDepartments = () => {
   const sql = `SELECT *
   FROM department
@@ -75,10 +73,10 @@ const queryDepartments = () => {
     console.table(data);
     mainMenu();
   });
-}
+};
 
-// Query all roles - formatted table showing job title, role id, the department that role belongs to, and the salary for that role
-// const allRoles ?
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 const queryRoles = () => {
   const sql = `SELECT title, salary, department.name
   FROM role
@@ -94,11 +92,10 @@ const queryRoles = () => {
     console.table(data);
     mainMenu();
   });
-}
+};
 
-
-// Query all employees - formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// const allEmployees ?
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 const queryEmployees = () => {
   const sql = `SELECT employee.id, first_name, last_name, role.title, role.salary, department.name AS department, manager_id AS manager FROM employee 
   JOIN role ON role.id = employee.role_id
@@ -113,54 +110,119 @@ const queryEmployees = () => {
     console.table(data);
     mainMenu();
   });
-}
+};
 
-// add department - employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// const addDepartment ?
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+const newDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department",
+        message: chalk.yellow(`Please provide new department name.`),
+      },
+    ])
+    .then((data) => {
+      db.query(
+        `INSERT INTO department (name) VALUES (?)`,
+        data.department,
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log(
+            chalk.red(`System has been updated with new department.`)
+          );
+          mainMenu();
+        }
+      );
+    });
+};
 
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+const newRole = () => {
+  db.query(`SELECT * FROM department`, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const depList = data.map((department) => ({
+      name: department.name,
+      value: department.id,
+    }));
 
-// add role - prompted to enter the name, salary, and department for the role and that role is added to the database
-// const addRole ?
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: chalk.yellow(`Please add name of new role.`)
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: chalk.yellow(`Enter yearly salary for new role.`)
+        },
+        {
+          type: "list",
+          name: "role",
+          message: chalk.yellow(`Enter department role will be apart of.`),
+          choices: depList
+        },
+      ])
+      .then((data) => {
+        db.query(
+          `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
+          [data.title, data.salary, data.role],
+          (err) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log(chalk.red(`System has been updated with new role`));
+            mainMenu();
+          }
+        );
+      });
+  });
+};
 
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+const newEmployee = () => {
 
-// add employee - prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// const addEmployee ?
-//Adding a new Employee to the database;
-const addEmployee = () => {
   inquirer.prompt([
     {
       type: "input",
       name: "firstName",
       message: chalk.yellow(`Please enter employee's first name?`),
-
     },
     {
       type: "input",
       name: "lastName",
       message: chalk.yellow(`Please enter employee's last name?`),
-
     },
     {
       type: "list",
       name: "title",
       message: chalk.yellow(`Please enter employee's title?`),
-
     },
     {
       type: "list",
       name: "manager",
       message: chalk.yellow(`Please enter employee's manager?`),
-
     },
   ]);
   console.log(chalk.red("A new employee was added to the database"));
   mainMenu();
 };
 
-// update employee - prompted to select an employee to update and their new role and this information is updated in the database
-// const updateEmployeeRole ?
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
-mainMenu()
+
+mainMenu();
 // .then(answers => {
 //   console.log(answers)
 // })
